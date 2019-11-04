8d8c005548acc652557a73376f1ebdf184bad006
Mumtaz <mumtaz.ahmed@e2enetworks.com>
2019-11-04 11:14:27 +0530

    initial setup

---
+++ src/app/services/adventure-time.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
+import { CHARACTERS } from './mock-data';
+
+@Injectable()
+export class AdventureTimeService {
+
+  constructor() { }
+
+  getCharacters(): Observable<any[]>{
+    return of(CHARACTERS).pipe(delay(100));
+  }
+
+  getColumns(): string[]{
+    return ["name", "age", "species", "occupation"]
+  }
+
+}
